a614b8f5f459518b8763f833163a642e8b68ef2d
SalwaAmalia <stsyalwa@gmail.com>
2022-08-31 10:29:07 +0700

    BDD App

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :registerable, :confirmable
+
+  has_many :books, :dependent => :delete_all
+end
